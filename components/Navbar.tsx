8879596b1f013b070b0132c6829cfdf6d553a3f1
Suwandy Wijaya <support@rahasia-dagang.com>
2026-05-17 00:00:58 +0700

    Make navbar logo 2x bigger

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
       <nav className="max-w-7xl mx-auto px-5 sm:px-8 lg:px-10 flex items-center justify-between h-[68px]">
         {/* Logo */}
         <a href="#beranda" className="flex items-center flex-shrink-0">
-          <Image src="/logo.png" alt="RahasiaDagang" width={140} height={48} className="h-10 w-auto object-contain" />
+          <Image src="/logo.png" alt="RahasiaDagang" width={280} height={96} className="h-20 w-auto object-contain" />
         </a>
 
         {/* Desktop links */}
